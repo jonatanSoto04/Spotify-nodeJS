b22ebd6474183446772e61659c70cf0275fa334b
Jonatan Soto <jonatan.sotop.04@gmail.com>
2025-04-29 18:49:45 -0500

    subiendo proyecto a git con conexion a base de datos y configuracion de librerias

---
+++ app.js
@@ -0,0 +1,17 @@
+//configuracion de express
+'use strict'
+// se cargan las librerias
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//configuracion de body parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// configuracion de cabeceras http
+
+// Rutas Base
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+//se carga las librerias
+const mongoose= require('mongoose');
+const app = require('./app');
+const port = process.env.PORT || 3977;
+//conexion a bd
+mongoose.connect('mongodb://localhost:27017/spotify_node')
+  .then(() => {
+    console.log('Conexión exitosa');
+    app.listen(port, function(){
+        console.log("Servidor del ApiREST de musica escuchando en http://localhost:" + port);
+    });
+  })    
+  .catch((err) => {
+    console.error('Error al conectar a MongoDB:', err);
+  });
+
+
